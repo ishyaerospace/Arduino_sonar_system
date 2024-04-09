1b8fd36d231e3bc473af7177b8abb69df63ecc39
ishyAerospace <119079770+ishyaerospace@users.noreply.github.com>
2024-04-09 12:36:55 +0100

    Update Radar_Code.ino

--- Radar_Code.ino
+++ Radar_Code.ino
@@ -18,14 +18,14 @@ void loop() {
   for(int i=15;i<=165;i++){  
   myServo.write(i);
   delay(30);
-  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  distance = calculateDistance();// Calls a function for calculating the distance
   
   Serial.print(i); // Sends the current degree into the Serial Port
-  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print(",");
   Serial.print(distance); // Sends the distance value into the Serial Port
-  Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print("."); 
   }
-  // Repeats the previous lines from 165 to 15 degrees
+  // Repeats the previous code from 165 to 15 degrees
   for(int i=165;i>15;i--){  
   myServo.write(i);
   delay(30);
@@ -36,12 +36,12 @@ void loop() {
   Serial.print(".");
   }
 }
-// Function for calculating the distance measured by the Ultrasonic sensor
+// calculates the distance measured bu ultrasonic sensor
 int calculateDistance(){ 
   
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
+  
   digitalWrite(trigPin, HIGH); 
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
